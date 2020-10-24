42048afb2b636617002773de02bcf10dbe9a755f
Gustavo <gustavo_-_luigi@hotmail.com>
2020-10-24 19:02:24 -0300

    Primeiro upload

---
+++ Prática de Design/Animações CSS JS/js/main.js
@@ -0,0 +1,14 @@
+function pulsaLua(){
+  let lua = document.getElementById("lua");
+  lua.classList.toggle("cresce");
+}
+
+function voaOvni(){
+  let ovni = document.getElementById("ovni");
+  ovni.classList.toggle("voa");
+}
+
+function giraAstronauta(){
+  let astronauta = document.getElementById("astronauta");
+  astronauta.classList.toggle("gira");
+}
